c81010a92cc6d6fb7249ad0652125ccafe26df3b
snider <snaydergp@hotmail.com>
2024-04-04 19:27:03 -0500

    correcion de estilos en  la pagina de inicio

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -2,11 +2,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage, RegisterPage } from '../pages'
 import { DailyCaloriesForm } from '../../DailyCaloriesForm/DailyCaloriesForm'
+import { SharedLayout } from '../../SharedLayout/SharedLayout'
 
 export const AuthRoutes = () => {
   return (
     <Routes>
-      <Route path='DailyCaloriesForm' element={<DailyCaloriesForm />} ></Route>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="DailyCaloriesForm" element={<DailyCaloriesForm />}></Route>
+      </Route> 
+      {/* <Route path='DailyCaloriesForm' element={<DailyCaloriesForm />} ></Route> */}
       <Route path='login' element={<LoginPage />}></Route>
       <Route path='register' element={<RegisterPage />}></Route>
       {/* Queda pendiente organizar con el companiero que cree la primera pagina para redireccionarlo ah[i] */}
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
